5e7e2fc61cca5933a244d64f43430ef000ed39ed
monsoontest1 <monsoontest1@gmail.com>
2021-09-18 17:21:32 +0530

    up

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
+---
+---
 if('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/sw.js')
+    navigator.serviceWorker.register('{{ site.url }}/sw.js')
     .then((reg) => console.log('sw registered', reg))
     .then((err) => console.log('sw not registered', err))
 }
